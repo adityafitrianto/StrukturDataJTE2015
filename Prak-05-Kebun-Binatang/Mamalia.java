cd3499b8c84329a13618c266d176b386faac5a97
Axio <adit.sepadu@gmail.com>
2015-09-12 19:46:01 +0700

    Praktikum 5

---
+++ Prak-05-Kebun-Binatang/Mamalia.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Mamalia here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Mamalia extends Binatang{
+    public int jumlahKaki();
+    
+}
---
+++ Prak-05-Kebun-Binatang/Reptilia.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Reptilia here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Reptilia extends Binatang
+{
+    public boolean merayap();
+}
