66c316037dbd71f45511425b908924acce647f4b
Axio <adit.sepadu@gmail.com>
2015-10-10 16:30:16 +0700

    Kuis-01-Rekam-Medik

---
+++ Kuis-01-Rekam-Medik/Pasien.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Pasien here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pasien
+{
+   private Pemeriksaan[] pemeriksaan;
+   private Catatan[] catatan;
+   
+   public Pasien(Pemeriksaan[] pemeriksaan){
+       this.pemeriksaan=pemeriksaan;
+       this.catatan=catatan;
+   }
+}
