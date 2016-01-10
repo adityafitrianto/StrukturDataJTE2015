b2eeebfb6e0d047ad9b44e5d694b324a590bf530
Aditya Fitrianto <adit.sepadu@gmail.com>
2016-01-11 00:19:33 +0700

    26-Final

---
+++ 26-Final/Node.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+
+public class Node
+{   
+    public Node (BigInteger data){
+        this.data=data;
+        this.berikut=null;
+    }
+    
+    public BigInteger data;
+    public Node berikut;
+}
